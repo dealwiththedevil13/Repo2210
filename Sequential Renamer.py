d090a7ed6800faea030c954a560cc9a93a47169f
Amanda Johnson <89553579+dealwiththedevil13@users.noreply.github.com>
2022-12-15 23:22:56 -0700

    Add files via upload

---
+++ Sequential Renamer.py
@@ -0,0 +1,15 @@
+import maya.cmds as cmds
+
+
+def Sequential_Renamer(string):
+    Select = cmds.ls(sl=True)
+    #The use of partition was a suggestion by Tanner King. Partitiion is used to create, query or add/remove sets to a partition. If a partition name needs to be specified, it is the first argument, other arguments represent the set names.#
+    string.partition("##")
+    for objNum, object in enumerate(Select):
+        cmds.rename(object,string.partition('#')[0] + str(objNum + 1).zfill(string.count('#')) + string.rpartition('#')[2])
+
+
+Sequential_Renamer("Arm_##_jnt")
+
+
+
